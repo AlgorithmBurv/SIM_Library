0864791d1b140c5082c007e0cf24278f9db35011
root <root@LAPTOP-9EFHH6T4>
2023-07-09 22:44:23 +0700

    first commit

--- src/resources/views/profile.blade.php
+++ src/resources/views/profile.blade.php
@@ -5,6 +5,8 @@
 @section('content')
     
 <div class="mt-5">
+    <h1>Welcome, {{ Auth::user()->username }}</h1>
+    <br><br>
     <h2>Your Rent Log</h2>
        <x-rent-log-table :rentlog='$rentlogs'/>
    </div>
